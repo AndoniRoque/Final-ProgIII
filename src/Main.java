fd1cc1a4ebc630d34378de6099d382d96ae52be9
AndoniRoque <roque.andoni@gmail.com>
2023-11-27 13:35:25 -0300

    create and delete logs file

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,43 @@
 //
-// Source code recreated from a .class file by IntelliJ IDEA
-// (powered by FernFlower decompiler)
+// Andoni Roque - Programación III - 2022
+// TUP - UTN - FRBB
 //
 
-import java.io.FileNotFoundException;
-import java.lang.reflect.Array;
-import java.util.*;
 import java.io.File;
-import java.io.PrintStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.util.*;
+
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
+
+
+    public static void main(String[] args) {
         System.out.println("Java Rol TUP");
         System.out.println("Welcome: ");
         String input = "0";
         ArrayList<Personaje> party_1 = new ArrayList();
         ArrayList<Personaje> party_2 = new ArrayList();
-        PrintStream logger = System.out;
 
+        File logs = new File("logs.txt");
+
+        try {
+            if (logs.createNewFile()) {
+                System.out.println("Logs file created " + logs.getName() + ".");
+                System.out.println("Current Working Directory: " + System.getProperty("user.dir"));
+            } else {
+                System.out.println("File already exists.");
+            }
+        } catch (IOException e) {
+            System.out.println("An error ocurred.");
+            e.printStackTrace();
+        }
+
+        try {
+            FileWriter writer = new FileWriter("logs.txt");
+
+        // Menú
         while(!input.equals("5")) {
             System.out.println("     1 . Start.");
             System.out.println("     2 . Start with random characters.");
@@ -25,88 +45,210 @@ public class Main {
             System.out.println("     4 . Delete log");
             System.out.println("     5 . Exit.");
 
-            Scanner scanner = new Scanner(System.in);
 
-            System.out.println("Select and press Enter: ");
 
-            input = scanner.nextLine();
 
-            if (input.equals("1")) {
 
-                // Generar 3 personajes por jugador
-                for(int i = 0; i < 3; i++) {
-                    party_1.add(GenerarPersonajes());
-                }
+                Scanner scanner = new Scanner(System.in);
 
-                System.out.println("Create second party...");
+                System.out.println("Select and press Enter: ");
 
-                for(int i = 0; i < 3; i++) {
-                    party_2.add(GenerarPersonajes());
-                }
-
-                System.out.println("-------------------------------");
-                System.out.println("Combat will start soon.");
-                System.out.println("-------------------------------");
-                System.out.println("Press Enter to continue...");
                 input = scanner.nextLine();
 
-                int i = 1; // Número de rounds.
+                if (input.equals("1")) {
 
-                while ( party_1.size() > 0 && party_2.size() > 0) {
-                    Combate(party_1, party_2, i);
-                }
+                    // Crear Personajes Manualmente.
 
-                if (party_1.isEmpty()){
+                    // Generar 3 personajes por jugador
+                    for (int i = 0; i < 3; i++) {
+                        party_1.add(GenerarPersonajes());
+                    }
+
+
+                    System.out.println("Press enter to continue...");
+                    input = scanner.nextLine();
+
+                    System.out.println("Create second party... press enter.");
+                    input = scanner.nextLine();
+
+                    for (int i = 0; i < 3; i++) {
+                        party_2.add(GenerarPersonajes());
+                    }
+
+                    System.out.println("-------------------------------");
+                    writer.write("-------------------------------\n");
+                    System.out.println("Combat will start soon.");
+                    writer.write("Combat will start soon.\n");
+                    System.out.println("-------------------------------");
+                    writer.write("-------------------------------\n");
                     System.out.println("Press Enter to continue...");
                     input = scanner.nextLine();
+
+                    int i = 1; // Número de rounds.
+
+                    while (party_1.size() > 0 && party_2.size() > 0) {
+                        Combate(party_1, party_2, i, writer);
+                    }
+
+                    if (party_1.isEmpty()) {
+                        System.out.println("Press Enter to continue...");
+                        input = scanner.nextLine();
+                        System.out.println("-------------------------------");
+                        writer.write("-------------------------------\n");
+                        System.out.println("CONGRATULATIONS PLAYER 2 - YOU'VE WON THE IRON THRONE.");
+                        writer.write("CONGRATULATIONS PLAYER 2 - YOU'VE WON THE IRON THRONE.\n");
+                        System.out.println("-------------------------------");
+                        writer.write("-------------------------------\n");
+                        System.out.println("^  ^  ^");
+                        writer.write("^  ^  ^\n");
+                        System.out.println("|V * V|");
+                        writer.write("|V * V|\n");
+                        System.out.println("|_____| ");
+                        writer.write("|_____|\n");
+                        writer.close();
+                    } else {
+                        System.out.println("Press Enter to continue...");
+                        input = scanner.nextLine();
+                        System.out.println("-------------------------------");
+                        writer.write("-------------------------------\n");
+                        System.out.println("CONGRATULATIONS PLAYER 1 - YOU'VE WON THE IRON THRONE.");
+                        writer.write("CONGRATULATIONS PLAYER 1 - YOU'VE WON THE IRON THRONE.\n");
+                        System.out.println("-------------------------------");
+                        writer.write("-------------------------------\n");
+                        System.out.println("^  ^  ^");
+                        writer.write("^  ^  ^\n");
+                        System.out.println("|V * V|");
+                        writer.write("|V * V|\n");
+                        System.out.println("|_____| ");
+                        writer.write("|_____|\n");
+                        writer.close();
+                    }
+
+                    party_1.clear();
+                    party_2.clear();
+
+
+                } else if (input.equals("2")) {
+
+                    // Crear personajes aleatoriamente.
+
+                    for (int i = 0; i < 3; i++) {
+                        party_1.add(GenerarPeronsajesAleatorios());
+                        party_2.add(GenerarPeronsajesAleatorios());
+                    }
+
                     System.out.println("-------------------------------");
-                    System.out.println("CONGRATULATIONS PLAYER 2 - YOU'VE WON THE IRON THRONE.");
+                    System.out.println("|          Party 1            |");
                     System.out.println("-------------------------------");
-                    System.out.println("^  ^  ^");
-                    System.out.println("|V * V|");
-                    System.out.println("|_____| ");
-                } else {
-                    System.out.println("Press Enter to continue...");
+                    writer.write("-------------------------------\n");
+                    writer.write("|          Party 1            |\n");
+                    writer.write("-------------------------------\n");
+                    MostrarParty(party_1);
+
+                    System.out.println("Press enter to continue...");
                     input = scanner.nextLine();
+
                     System.out.println("-------------------------------");
-                    System.out.println("CONGRATULATIONS PLAYER 1 - YOU'VE WON THE IRON THRONE.");
+                    System.out.println("|          Party 2            |");
                     System.out.println("-------------------------------");
-                    System.out.println("^  ^  ^");
-                    System.out.println("|V * V|");
-                    System.out.println("|_____| ");
+                    writer.write("-------------------------------\n");
+                    writer.write("|          Party 2            |\n");
+                    writer.write("-------------------------------\n");
+                    MostrarParty(party_2);
+
+                    System.out.println("Press enter to continue...");
+                    input = scanner.nextLine();
+
+                    int i = 1; // Número de rounds.
+
+                    while (party_1.size() > 0 && party_2.size() > 0) {
+                        Combate(party_1, party_2, i);
+                    }
+
+                    if (party_1.isEmpty()) {
+                        System.out.println("Press Enter to continue...");
+                        input = scanner.nextLine();
+                        System.out.println("-------------------------------");
+                        System.out.println("CONGRATULATIONS PLAYER 2 - YOU'VE WON THE IRON THRONE.");
+                        System.out.println("-------------------------------");
+                        writer.write("-------------------------------\n");
+                        writer.write("CONGRATULATIONS PLAYER 2 - YOU'VE WON THE IRON THRONE.\n");
+                        writer.write("-------------------------------\n");
+                        System.out.println("^  ^  ^");
+                        writer.write("^  ^  ^\n");
+                        System.out.println("|V * V|");
+                        writer.write("|V * V|\n");
+                        System.out.println("|_____| ");
+                        writer.write("|_____|\n");
+                        writer.close();
+                    } else {
+                        System.out.println("Press Enter to continue...");
+                        input = scanner.nextLine();
+                        System.out.println("-------------------------------");
+                        System.out.println("CONGRATULATIONS PLAYER 1 - YOU'VE WON THE IRON THRONE.");
+                        System.out.println("-------------------------------");
+                        writer.write("-------------------------------\n");
+                        writer.write("CONGRATULATIONS PLAYER 1 - YOU'VE WON THE IRON THRONE.\n");
+                        writer.write("-------------------------------\n");
+                        System.out.println("^  ^  ^");
+                        writer.write("^  ^  ^\n");
+                        System.out.println("|V * V|");
+                        writer.write("|V * V|\n");
+                        System.out.println("|_____| ");
+                        writer.write("|_____|\n");
+                        writer.close();
+                    }
+
+                    party_1.clear();
+                    party_2.clear();
+
+
+                    break;
+
+                } else if (input.equals("3")) {
+                    // TODO: mostrar logs.
+                    // Mostrar Logs
+
+                    break;
+
+                } else if (input.equals("4")) {
+
+                    // Borrar logs.
+
+                    System.out.println("Are you sure you want to delete the logs?");
+                    System.out.println("1 - YES");
+                    System.out.println("2 - Cancel.");
+                    input = scanner.nextLine().toUpperCase();
+
+                    if(input.equals("1") || input.equals("YES")) {
+                        if (logs.exists()) {
+                            if (logs.delete()) {
+                                System.out.println("Logs file deletated successfully.");
+                            } else {
+                                System.out.println("Unable to delte the file.");
+                            }
+                        } else {
+                            System.out.println("File doesn't exist.");
+                        }
+                    }
+                    break;
+
+                } else if (!input.equals("5")) {
+                    System.out.println("You shall not pass!!");
                 }
 
-                party_1.clear();
-                party_2.clear();
+                System.out.println("Press Enter to exit...");
+                scanner.nextLine();
 
 
-
-            } else if (input.equals("2")) {
-                // TODO: generar partys aleatorias.
-
-                break;
-
-            } else if (input.equals("3")) {
-                // TODO: mostrar logs.
-                PrintStream Log = new PrintStream(new File("logs.txt"));
-                System.setOut(Log);
-                break;
-
-            } else if (input.equals("4")) {
-                // TODO: borrar logs.
-                break;
-
-            } else if (!input.equals("5")){
-                System.out.println("You shall not pass!!");
             }
-
-            System.out.println("Press Enter to exit...");
-            scanner.nextLine();
+        } catch (IOException e){
+            System.out.println("An error occurred.");
+            e.printStackTrace();
         }
-
-        System.setOut(logger);
-
     }
+
+
 
     public static Personaje GenerarPersonajes() {
         // TODO: limitar skill points
@@ -208,7 +350,7 @@ public class Main {
         return party.get(jugador);
     }
 
-    public static void Combate(ArrayList<Personaje> party_1, ArrayList<Personaje> party_2, int i) {
+    public static void Combate(ArrayList<Personaje> party_1, ArrayList<Personaje> party_2, int i, Writer writer) throws IOException {
         Scanner scanner = new Scanner(System.in);
         String input = "";
         ArrayList <Personaje> first_attacker = (ArrayList<Personaje>) ElegirPartyAtaca(party_1, party_2);
@@ -216,31 +358,42 @@ public class Main {
 
         if (first_attacker.equals(party_1)) {
             System.out.println("First party attacks first.");
+            writer.write("First party attacks first.\n");
             second_attacker = party_2;
         } else {
             System.out.println("Second party attacks first");
+            writer.write("Second party attacks first\n");
             second_attacker = party_1;
         }
 
         System.out.println("-------------------------------");
+        writer.write("-------------------------------\n");
         Personaje attacker = ElegirPersonaje(first_attacker);
         Personaje defender = ElegirPersonaje(second_attacker);
         System.out.println(">>>" + attacker.name + " attacks first.");
+        writer.write(">>>" + attacker.name + " attacks first.\n");
         System.out.println("<<<" + defender.name + " will defend.");
+        writer.write("<<<" + attacker.name + " will defend.\n");
 
         while(i < 7 && attacker.getHealth() > 0 && defender.getHealth() > 0) {
             System.out.println("-------------------------------");
+            writer.write("-------------------------------\n");
             System.out.println("Round: " + i);
+            writer.write("Round: " + i + "\n");
             System.out.println("-------------------------------");
+            writer.write("-------------------------------\n");
 
             if (attacker instanceof Humano ) {
                 double danio = ((Humano) attacker).ataque_humano(defender);
                 System.out.println(attacker.name + " attacks with: " + danio + " points to " + defender.name + ".");
+                writer.write(attacker.name + " attacks with: " + danio + " points to " + defender.name + ".\n");
                 defender.health_update(danio);
                 System.out.println("Press Enter to continue...");
                 input = scanner.nextLine();
                 System.out.println("La salud del atacante: " + attacker.getHealth() + ".");
                 System.out.println("La salud del defensor: " + defender.getHealth() + ".");
+                writer.write("La salud del atacante: " + attacker.getHealth() + ".\n");
+                writer.write("La salud del defensor: " + defender.getHealth() + ".\n");
                 System.out.println("Press Enter to continue...");
                 input = scanner.nextLine();
             } else if (attacker instanceof Orco) {
@@ -363,4 +516,62 @@ public class Main {
             }
         }
     }
+
+    public static Personaje GenerarPeronsajesAleatorios() {
+
+        Random random = new Random();
+
+        int tipo_select = random.nextInt(3);
+        String tipo;
+
+        if (tipo_select == 0) {
+            tipo = "Humano";
+        } else if (tipo_select == 1) {
+            tipo = "Orco";
+        } else {
+            tipo = "Elfo";
+        }
+
+        int str = random.nextInt(5) + 1;
+        int dex = random.nextInt(5) + 1;
+        int def = random.nextInt(5) + 1;
+        int spd = random.nextInt(5) + 1;
+
+        Personaje character_1 = null;
+
+        List<NamesHumano> NombresHumanosList = Arrays.asList(NamesHumano.values());
+        Collections.shuffle(NombresHumanosList);
+        String nameHumano = String.valueOf(NombresHumanosList.get(0));
+
+        List<NamesOrco> NombresOrcosList = Arrays.asList(NamesOrco.values());
+        Collections.shuffle(NombresOrcosList);
+        String nameOrco = String.valueOf(NombresOrcosList.get(0));
+
+        List<NamesElfo> NombresElfosList = Arrays.asList(NamesElfo.values());
+        Collections.shuffle(NombresElfosList);
+        String nameElfo = String.valueOf(NombresElfosList.get(0));
+
+        if (tipo.equals("Humano")) {
+            character_1 = new Humano(10, 100, nameHumano , str, dex, def, spd, tipo);
+        } else if (tipo.equals("Orco")) {
+            character_1 = new Orco (10, 100, nameOrco, str, dex, def, spd, tipo);
+        } else {
+            character_1 = new Elfo(10, 100, nameElfo, str, dex, def, spd, tipo);
+        }
+
+        return character_1;
+    }
+
+    public static void MostrarParty(ArrayList<Personaje> party) {
+        for (Personaje personajes : party) {
+            System.out.println("| " + personajes.name + "  -  " + personajes.tipo + ", lvl: " + personajes.level);
+            System.out.println("|    Strength: " + personajes.str + ".");
+            System.out.println("|    Dexterity: " + personajes.dex + ".");
+            System.out.println("|    Defence: " + personajes.def + ".");
+            System.out.println("|    Speed: " + personajes.spd + ".");
+            System.out.println("-------------------------------");
+
+        }
+    }
+
 }
